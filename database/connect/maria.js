a79641c47639c3f2b62f2e91d2f37465fc313cf1
dnwn-9001 <j10511@hanmail.net>
2022-07-12 03:50:53 +0900

    maria.js 수정

--- database/connect/maria.js
+++ database/connect/maria.js
@@ -2,7 +2,7 @@ const maria = require("mysql");
 
 const conn = maria.createConnection({
   host: "127.0.0.1",
-  port: 3306,
+  port: process.env.PORT || 3306,
   user: "root",
   password: "wkdduddls",
   database: "before_we_meet",
