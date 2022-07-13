8bb43764f3646ff2a4c1e5167b7019f57db3de97
dnwn-9001 <j10511@hanmail.net>
2022-07-13 22:58:16 +0900

    chat.js port 수정

--- chat.js
+++ chat.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
-
-app.set("port", process.env.PORT || 443 || 4000);
-
 const httpServer =
   process.env.NODE_ENV === "production"
     ? require("https").createServer({
