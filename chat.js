9000e6698d08bfa930e052a5a4907ad395c90c80
dnwn-9001 <j10511@hanmail.net>
2022-07-13 20:03:54 +0900

    chat.js 수정

--- chat.js
+++ chat.js
@@ -1,11 +1,19 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
 const httpServer =
   process.env.NODE_ENV === "production"
     ? require("https").createServer({
         key: fs.readFileSync("/tmp/key.pem"),
         cert: fs.readFileSync("/tmp/cert.pem"),
       })
-    : require("http").createServer();
+    : require("http").createServer(app);
 const port = process.env.NODE_ENV === "production" ? 443 : 8082;
+
+app.get("/", (req, res) => {
+  res.send("chat!!");
+});
+
 const io = require("socket.io")(httpServer, {
   cors: {
     origin:
